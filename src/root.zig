31428e3d630f9d796b4345219163e7808c45b11c
moortu <codeberg.compacted096@passmail.net>
2026-02-28 17:04:45 +0100

    Revert "fixed issue with shadowed vars"
    This reverts commit 1aa805ff82aa3c979afd6db71d0c8777947e09a0.

--- src/root.zig
+++ src/root.zig
@@ -1618,14 +1618,14 @@ pub const ClaimedInterface = struct {
 
         fn readFn(context: *const anyopaque, buffer: []u8) Error!usize {
             const self: *const Readable = @ptrCast(@alignCast(context));
-            var bytes_read: c_int = 0;
-            c.libusb_bulk_transfer(self.device_handle, self.endpoint, buffer.ptr, @intCast(buffer.len), &bytes_read, self.timeout).result() catch |err| {
+            var read: c_int = 0;
+            c.libusb_bulk_transfer(self.device_handle, self.endpoint, buffer.ptr, @intCast(buffer.len), &read, self.timeout).result() catch |err| {
                 if (err != error.OperationTimedOut) {
                     return err;
                 }
             };
 
-            return @intCast(bytes_read);
+            return @intCast(read);
         }
 
         pub fn reader(self: *const Readable) std.io.AnyReader {
